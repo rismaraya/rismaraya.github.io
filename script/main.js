fb35685209cdfc07a50a7418caecb84802d3f42d
khairulhabibie <khairulhabibie01@gmail.com>
2022-04-02 19:12:17 +0700

    menambahkan link pada section kegiatan

--- script/main.js
+++ script/main.js
@@ -6,3 +6,14 @@ alert.addEventListener('click', function () {
     text: 'Halamn web belum tersedia',
   });
 });
+
+const alert2 = document.querySelectorAll('.errorAlert2');
+for (let i = 0; i < alert2.length; i++) {
+  alert2[i].addEventListener('click', function () {
+    Swal.fire({
+      icon: 'error',
+      title: 'Akses Gagal !',
+      text: 'File belum tersedia',
+    });
+  });
+}
